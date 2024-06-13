5be397e83168740add2b44dc42596017328e4ba0
Phil Schumann <metaleap@users.noreply.github.com>
2024-06-13 20:29:50 +0200

    initial scaffold cont.

--- src/lsp.ts
+++ src/lsp.ts
@@ -1,8 +1,27 @@
 import * as vsc from 'vscode';
 
 
-export function textDocument_hover(td: vsc.TextDocument, pos: vsc.Position, cancel: vsc.CancellationToken): vsc.ProviderResult<vsc.Hover> {
-    return {
-        contents: ['Hover Content']
-    }
+import * as lsp from 'vscode-languageclient/node';
+
+
+export type Client = lsp.LanguageClient
+
+
+
+export function init(ctx: vsc.ExtensionContext): lsp.LanguageClient {
+    const client = new lsp.LanguageClient(
+        'lsp_gerbil', 'Gerbil LSP',
+
+        {
+            command: "gxlsp",
+        } as lsp.ServerOptions,
+
+        {
+            documentSelector: [{ language: 'gerbil', scheme: 'file' }],
+            synchronize: { fileEvents: vsc.workspace.createFileSystemWatcher('**/*.ss') },
+        } as lsp.LanguageClientOptions
+
+    )
+    client.start()
+    return client
 }
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,22 @@ import * as vsc from 'vscode';
 import * as lsp from './lsp'
 
 
+let lsp_client: lsp.Client;
 
-export function activate(context: vsc.ExtensionContext) {
-	context.subscriptions.push(...[
 
-		vsc.languages.registerHoverProvider('gerbil', {
-			provideHover: lsp.textDocument_hover,
-		}),
+export function activate(ctx: vsc.ExtensionContext) {
+	lsp_client = lsp.init(ctx)
 
-	]);
+	ctx.subscriptions.push(...[
+		lsp_client,
+		// other, future disposables go here...
+	])
+
 }
 
-export function deactivate() { }
+export function deactivate() {
+	if (lsp_client)
+		return lsp_client.stop()
+
+	return void 0
+}
