5402d3a70a14e9838d401f0f8fe9c4dcf892a12f
Phil Schumann <metaleap@users.noreply.github.com>
2024-06-28 18:31:36 +0200

    build-on-save

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,64 @@
 import * as vsc from 'vscode'
 import * as lsp from './lsp'
 import * as repl from './repl'
+import * as node_path from 'node:path'
+import * as node_fs from 'node:fs'
+import * as node_exec from 'child_process'
 
 
-let lsp_client: lsp.Client | null = null
+let lspClient: lsp.Client | null = null
+let statusBarItemBuildOnSave: vsc.StatusBarItem
 
 
 export function activate(ctx: vsc.ExtensionContext) {
+	// register "repl", aka vscode custom notebook type
 	ctx.subscriptions.push(vsc.workspace.registerNotebookSerializer("gerbil-repl", new repl.NotebookSerializer()))
 	ctx.subscriptions.push(new repl.Kernel())
 
-	lsp_client = lsp.init(ctx)
+	// bring up LSP client unless disabled in user config
+	lspClient = lsp.init(ctx)
+	if (lspClient)
+		ctx.subscriptions.push(lspClient)
 
-	if (lsp_client)
-		ctx.subscriptions.push(lsp_client)
+	// set up build-on-save
+	ctx.subscriptions.push(vsc.workspace.onDidSaveTextDocument(tryBuildOnSave))
+	ctx.subscriptions.push(statusBarItemBuildOnSave =
+		vsc.window.createStatusBarItem("gerbil-build-on-save", vsc.StatusBarAlignment.Left))
+	statusBarItemBuildOnSave.text = "$(coffee)"
+	statusBarItemBuildOnSave.tooltip = "Gerbil build-on-save running..."
 }
 
 export function deactivate() {
-	if (lsp_client)
-		return lsp_client.stop()
+	if (lspClient)
+		return lspClient.stop()
 
 	return (void 0)
+}
+
+
+function tryBuildOnSave(justSaved: vsc.TextDocument) {
+	const cfg = vsc.workspace.getConfiguration()
+	const build_on_save = cfg.get<boolean>("gerbil.buildOnSave", false)
+	if (!build_on_save)
+		return
+
+	let dir_path = node_path.dirname(justSaved.fileName)
+	let pkg_file_path = node_path.join(dir_path, "gerbil.pkg")
+	while ((dir_path !== "/") && !node_fs.existsSync(pkg_file_path)) {
+		dir_path = node_path.dirname(dir_path)
+		pkg_file_path = node_path.join(dir_path, "gerbil.pkg")
+	}
+	if (!node_fs.existsSync(pkg_file_path))
+		return
+
+	statusBarItemBuildOnSave.show()
+	setTimeout(() => { // needed for the status-item to actually show, annoyingly
+		try {
+			const buf = node_exec.execFileSync("gerbil", ["build"], { cwd: dir_path, })
+		} catch (err) {
+			vsc.window.showErrorMessage("Build-on-Save failed: " + err)
+		} finally {
+			statusBarItemBuildOnSave.hide()
+		}
+	}, 1)
 }
--- src/repl.ts
+++ src/repl.ts
@@ -8,7 +8,7 @@ import * as vsc from 'vscode'
 
 export class Kernel {
     private readonly self: vsc.NotebookController
-    private exec_order = 0
+    private execOrder = 0
 
     constructor() {
         this.self = vsc.notebooks.createNotebookController("nbctl-gerbil-repl", "gerbil-repl", "Gerbil REPL")
@@ -25,7 +25,7 @@ export class Kernel {
         for (const cell of cells)
             if (cell.kind === vsc.NotebookCellKind.Code) {
                 const exec = this.self.createNotebookCellExecution(cell)
-                exec.executionOrder = ++this.exec_order
+                exec.executionOrder = ++this.execOrder
                 exec.start(Date.now())
                 const src = cell.document.getText().trim()
                 // TODO: actual REPL / eval call here
@@ -43,22 +43,22 @@ export class Kernel {
 
 interface NotebookCell {
     source: string
-    cell_type: vsc.NotebookCellKind
+    cellType: vsc.NotebookCellKind
 }
 
 export class NotebookSerializer implements vsc.NotebookSerializer {
     async serializeNotebook(data: vsc.NotebookData, _: vsc.CancellationToken): Promise<Uint8Array> {
         const cells: NotebookCell[] = []
         for (const cell of data.cells)
-            cells.push({ source: cell.value, cell_type: cell.kind })
+            cells.push({ source: cell.value, cellType: cell.kind })
         return new TextEncoder().encode(JSON.stringify(cells))
     }
 
     async deserializeNotebook(content: Uint8Array, _: vsc.CancellationToken): Promise<vsc.NotebookData> {
         const cells = (<NotebookCell[]>JSON.parse(new TextDecoder().decode(content)))
         return new vsc.NotebookData(cells.map(item =>
-            new vsc.NotebookCellData(item.cell_type, item.source,
-                ((item.cell_type === vsc.NotebookCellKind.Code) ? 'gerbil' : 'markdown'))
+            new vsc.NotebookCellData(item.cellType, item.source,
+                ((item.cellType === vsc.NotebookCellKind.Code) ? 'gerbil' : 'markdown'))
         ))
     }
 }
